47142de08ed4f6d0f498e99275d984ef496bdcdc
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-02-05 21:08:08 +0300

    Edit tests

--- src/test/java/tests/DragAndDrop.java
+++ src/test/java/tests/DragAndDrop.java
@@ -3,18 +3,19 @@ package tests;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @Description("Перенести прямоугольник А на место В")
     @Test
-    void SoftAssertionsTest() {
+    void swapABTest() {
         //не работает
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo($("#column-b"));
-
+        $$("#columns").shouldHave(exactTexts("B A"));
 //тоже не работает
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(300,200).release().perform();
-
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(300, 200).release().perform();
+        $$("#columns").shouldHave(exactTexts("B A"));
     }
 }
--- src/test/java/tests/GithubSoftAssertionsPage.java
+++ src/test/java/tests/GithubSoftAssertionsPage.java
@@ -4,18 +4,19 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GithubSoftAssertionsPage {
 
     @Description("Открыть страницу SoftAssertions в github/selenide и проверить что внутри есть пример кода для JUnit5")
     @Test
-    void SoftAssertionsTest() {
+    void softAssertionsTest() {
         open("https://github.com/");
         $("[name=q]").val("Selenide").pressEnter();
-        $$("ul.repo-list li").first().$("a").click();
+        $("ul.repo-list li").$("a").click();
         $("h1").shouldHave(text("selenide / selenide"));
-        $("[data-hotkey='g w']").click();
+        $(byText("Wiki")).click();
         $("#wiki-body").shouldHave(text("Welcome to the selenide wiki!"));
         $$(".Box-row").findBy(text("SoftAssertions")).click();
         $(".page").shouldHave(text("Using JUnit5 extend test class:"));
